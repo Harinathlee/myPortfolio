186a55e38d9433efb34e6b1a4d0eea9a1257565d
Harinathlee <harinathlee2598@gmail.com>
2022-03-09 23:30:07 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './style.css';
+import Topbar from './topbar/Topbar';
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Topbar></Topbar>
+    </div>
+  );
 }
--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -1,11 +1,20 @@
+import React from 'react';
 import './Topbar.scss';
 
 export default function about() {
-  const topbarElement = (
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
+  const topbarNav = (
+    <div>
+      <h2 class="Title">KnowMe</h2>
+
+      <nav class="navMenu">
+        <a href="#">Home</a>
+        <a href="#">AboutMe</a>
+        <a href="#">Work</a>
+        <a href="#">ContactMe</a>
+        <div class="dot"></div>
+      </nav>
+    </div>
   );
+
+  return topbarNav;
 }
